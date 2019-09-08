b5981c77855175fc380a0ebb1953772dd1baa622
Jake Runzer <jakerunzer@gmail.com>
2019-09-08 15:17:35 +0100

    randomizer module

---
+++ test/quiztastic/randomizer_test.exs
@@ -0,0 +1,41 @@
+defmodule Quiztastic.RandomizerTest do
+  use ExUnit.Case
+
+  alias Quiztastic.Randomizer
+
+  test "generate random string" do
+    random = Randomizer.randomizer(100)
+    assert 100 = String.length(random)
+    assert String.match?(random, ~r/[A-Za-z0-9]/)
+  end
+
+  test "generate random string of different length" do
+    random = Randomizer.randomizer(2)
+    assert 2 = String.length(random)
+    assert String.match?(random, ~r/[A-Za-z0-9]/)
+  end
+
+  test "generate random numbers" do
+    random = Randomizer.randomizer(100, :numeric)
+    assert 100 = String.length(random)
+    refute String.match?(random, ~r/[A-Za-z]/)
+  end
+
+  test "generate random upper case" do
+    random = Randomizer.randomizer(100, :upcase)
+    assert 100 = String.length(random)
+    refute String.match?(random, ~r/[a-z0-9]/)
+  end
+
+  test "generate random lower case" do
+    random = Randomizer.randomizer(100, :downcase)
+    assert 100 = String.length(random)
+    refute String.match?(random, ~r/[A-Z0-9]/)
+  end
+
+  test "generate random alphabets only" do
+    random = Randomizer.randomizer(100, :alpha)
+    assert 100 = String.length(random)
+    refute String.match?(random, ~r/[0-9]/)
+  end
+end
